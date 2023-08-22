284f533f7f1b8537eea17ed3310440515a3a8b93
ohAkse <segassdc1@naver.com>
2023-08-22 15:16:23 +0900

    Initial Commit

---
+++ Memo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo2
+//
+//  Created by 박유경 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view. 
+    }
+
+
+}
+
